577164b80e82f60fa45d3365752717b918099c65
DiegoKrause1 <diego.emanuel.krause@gmail.com>
2019-03-12 19:37:21 -0300

    ionic

--- primeiroProjeto/src/pages/home/home.ts
+++ primeiroProjeto/src/pages/home/home.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Messages } from '../../providers/messages';
+import { ToastController } from 'ionic-angular';
+import { Toast } from '../../providers/toast';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  
 }
---
+++ primeiroProjeto/src/providers/toast.ts
@@ -0,0 +1,20 @@
+import { ToastController } from 'ionic-angular';
+import { Injectable } from '../../node_modules/@angular/core';
+
+@Injectable()
+export class Toast {
+
+  constructor(public toastCtrl: ToastController) { }
+
+  presentToast(toastMessage : string) {
+    const toast = this.toastCtrl.create({
+      message: toastMessage,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  public loadingShow(toastMessage : string = "Carregando"){
+    this.presentToast(toastMessage);
+  }
+}
